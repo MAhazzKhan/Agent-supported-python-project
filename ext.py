0c43566dc9b68cc46bbf872cefaedbd63e326df9
MAhazzKhan <81685737+MAhazzKhan@users.noreply.github.com>
2023-09-01 19:33:01 +0000

    change

--- ext.py
+++ ext.py
@@ -7,6 +7,9 @@ class car:
   def __str__(self):
     return f'The {self.make} {self.model}\'s {self.varient}'
 
+  def move(self,make,model):
+    return f'The {self.make} {self.model} is moving'
+
 class seats(car):
   def __init__(self,make,model,varient,type):
     super().__init__(make,model,varient)
@@ -14,3 +17,21 @@ class seats(car):
 
   def __str__(self):
     return f'The {self.make} {self.model}\'s {self.varient} has {self.type} seats'
+
+
+class plane:
+  def __init__(self,make,model):
+    self.make = make  
+    self.model = model
+
+  def move(self,make,model):
+    return f'The {self.make} {self.model} is moving'
+
+class bike:
+  def __init__(self,make,model):
+    self.make = make  
+    self.model = model
+
+  def move(self,make,model):
+    return f'The {self.make} {self.model} is moving'
+
--- main.py
+++ main.py
@@ -9,3 +9,11 @@ new2 = car(new.make,"F-250","Raptor-R")
 new3 = seats(new2.make ,new2.model ,new2.varient ,"carbon fiber bucket")
 
 print(new3)
+
+
+car1 = new2
+plane1 = plane("Boeing","747")
+bike1 = bike("Kawasaki","ninja H2R")
+
+for i in(car1,plane1,bike1):
+  print(i.make,i.model)
