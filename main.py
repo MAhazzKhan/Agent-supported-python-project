e8bc9d00a59d9d09f9e561b154f82679aa48b34e
MAhazzKhan <81685737+MAhazzKhan@users.noreply.github.com>
2023-09-01 20:02:40 +0000

    change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from ext import *
+
+
 new = car("Ford","Mustang","GT3")
 a = new.make
 del new.model
@@ -9,3 +11,11 @@ new2 = car(new.make,"F-250","Raptor-R")
 new3 = seats(new2.make ,new2.model ,new2.varient ,"carbon fiber bucket")
 
 print(new3)
+print(new3)
+
+car1 = new2
+plane1 = plane("Boeing","747")
+bike1 = bike("Kawasaki","ninja H2R")
+
+for i in(car1,plane1,bike1):
+  print(f'{i.make} {i.model}')
